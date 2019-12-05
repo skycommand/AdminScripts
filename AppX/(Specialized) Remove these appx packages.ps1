53cd44c226423f751c11f1c5379a06f3336e813c
skycommand <jadangerbrooks@hotmail.com>
2019-12-05 16:15:32 +0330

    Add files via upload

---
+++ AppX/(Specialized) Remove these appx packages.ps1
@@ -0,0 +1,56 @@
+Import-Module -Name Appx -ErrorAction Stop
+
+$applist = @(
+  "Microsoft.3DBuilder",                      # 3D Builder
+  "Microsoft.DesktopAppInstaller",            # App Installer 
+  "Microsoft.GetHelp",                        # Get Help
+  "Microsoft.Getstarted",                     # Tips
+  "Microsoft.Messaging",                      # Messaging
+  "Microsoft.Microsoft3DViewer",              # 3D Viewer (formerly Mixed Reality Viewer, View 3D)
+  "Microsoft.MicrosoftOfficeHub",             # My Office
+  "Microsoft.MicrosoftSolitaireCollection",   # Solitaire Collection
+  "Microsoft.MixedReality.Portal",            # Mixed Reality Portal
+  "Microsoft.MSPaint",                        # Paint 3D
+  "Microsoft.Office.OneNote",                 # OneNote (the pathetic remake, not the original)
+  "Microsoft.OneConnect",                     # Paid Wi-Fi & Cellular
+  "Microsoft.Print3D",                        # Print 3D
+  "Windows.Print3D",                          # Print 3D backend
+  "Microsoft.StorePurchaseApp",               # Store Purchase App
+  "Microsoft.Wallet",                         # Pay
+  "Microsoft.WindowsFeedbackHub",             # Feedback Hub
+  "Microsoft.WindowsSoundRecorder",           # Voice Recorder
+  "Microsoft.Xbox.TCUI",                      # Xbox Live in-game experience
+  "Microsoft.XboxApp",                        # Xbox Console Companion (formerly Xbox)
+  "Microsoft.XboxGameOverlay",                # Xbox Game Bar Plug-in
+  "Microsoft.XboxGamingOverlay",              # Xbox Game Bar
+  "Microsoft.XboxIdentityProvider",           # Xbox Identity Provider
+  "Microsoft.XboxSpeechToTextOverlay",        # ???
+  "Microsoft.YourPhone",                      # Your Phone
+  "Microsoft.ZuneMusic",                      # Groove
+  "Microsoft.ZuneVideo",                      # Movies & TV
+  "Windows.ContactSupport",                   # Contact Support
+  "ActiproSoftwareLLC.562882FEEB491",         # Code Writer
+  "46928bounde.EclipseManager",               # Eclipse Manager
+  "PandoraMediaInc.29680B314EFC2",            # Pandora
+  "AdobeSystemIncorporated. AdobePhotoshop",  # Adobe Photoshop Express
+  "D5EA27B7.Duolingo- LearnLanguagesforFree", # Duolingo
+  "Microsoft.NetworkSpeedTest",               # Network Speed Test
+  "Microsoft.BingNews",                       # Bing News
+  "Microsoft.Office.Sway"                     # Office Sway
+)
+
+foreach ($app in $applist) {
+  Write-Output $("Looking for {0}" -f $app)
+  $APPXs = Get-AppxPackage $app -AllUsers
+  if ($null -ne $APPXs )
+  {
+    foreach ($APPX in $APPXs) {
+      Write-Output $("Removing {0}" -f $APPX.ToString())
+      Remove-AppxPackage -AllUsers -Package $APPX
+    }
+    Remove-Variable APPX
+  } else {
+    Write-Output $("{0} is not installed." -f $app)
+  }
+  Write-Output ""
+}
---
+++ AppX/Inventory AppX Packages.ps1
@@ -0,0 +1,100 @@
+﻿[CmdletBinding()]
+param (
+)
+
+<# 
+C# code to expose SHLoadIndirectString(), derived from:
+	Title:  Expand-IndirectString.ps1
+	Author: Jason Fossen, Enclave Consulting LLC (www.sans.org/sec505)
+	Date:   20 September 2016
+	URL:    https://github.com/SamuelArnold/StarKill3r/blob/master/Star%20Killer/Star%20Killer/bin/Debug/Scripts/SANS-SEC505-master/scripts/Day1-PowerShell/Expand-IndirectString.ps1
+
+Jason has released his code to public domain.
+#>
+$CSharpSHLoadIndirectString = @'
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+public class IndirectStrings
+{
+	[DllImport("shlwapi.dll", BestFitMapping = false, CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = false, ThrowOnUnmappableChar = true)]
+	internal static extern int SHLoadIndirectString(string pszSource, StringBuilder pszOutBuf, uint cchOutBuf, IntPtr ppvReserved);
+
+	public static string GetIndirectString(string indirectString)
+	{
+		StringBuilder lptStr = new StringBuilder(1024);
+		int returnValue = SHLoadIndirectString(indirectString, lptStr, (uint)lptStr.Capacity, IntPtr.Zero);
+
+		return returnValue == 0 ? lptStr.ToString() : null;
+	}
+}
+'@
+
+# Add the IndirectStrings type to PowerShell
+Add-Type -TypeDefinition $CSharpSHLoadIndirectString -Language CSharp
+
+<#
+Usage examples:
+
+	$instr1 = '@%SystemRoot%\system32\shell32.dll,-21801'
+	$instr2 = '@{This.is.deliberately.invalid}'
+	$instr3 = '@{c5e2524a-ea46-4f67-841f-6a9465d9d515_10.0.18362.267_neutral_neutral_cw5n1h2txyewy?ms-resource://FileExplorer/Resources/AppxManifest_DisplayName}'
+
+	[IndirectStrings]::GetIndirectString( $instr1 ) 
+	[IndirectStrings]::GetIndirectString( $instr2 )
+	[IndirectStrings]::GetIndirectString( $instr3 ) 
+#>
+
+# Get a list of Appx packages
+$AppxPackages = Get-AppxPackage -Verbose
+$AppxSum = $AppxPackages.Count
+
+# Create an array to store Appx identities
+Class AppxIdentity {
+	[ValidateNotNullOrEmpty()][string]$Name
+	[string]$DisplayNameResolved
+	[string]$DisplayNameRaw
+}
+[AppxIdentity[]]$AppxIdentities = [AppxIdentity[]]::New($AppxSum)
+
+# Access the AppX repository in the Registry
+Push-Location "Registry::HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\Repository\Packages"
+
+for ($i = 0; $i -lt $AppxSum; $i++) {
+	# These variables help make the code more compact
+	# AXN, AXF and AXI respectively mean AppX Name, AppX Fullname and AppX Identity
+	$AXN = $AppxPackages[$i].Name
+	$AXF = $AppxPackages[$i].PackageFullName
+	$AXI = New-Object -TypeName AppxIdentity
+
+	# The first property is easy to acquire
+	$AXI.Name = $AXN
+ 
+	#The display name is stored in the Registry
+	If (Test-Path $AXF) {
+		try {
+			$AXI.DisplayNameRaw = (Get-ItemProperty -Path $AXF -Name DisplayName).DisplayName
+			if ($AXI.DisplayNameRaw -match '^@') {
+				$AXI.DisplayNameResolved = [IndirectStrings]::GetIndirectString( $AXI.DisplayNameRaw )
+				if ($AXI.DisplayNameResolved -eq '') {
+					Write-Warning "$($AXN): Could not resolve the display name."
+				}
+			} else {
+				$AXI.DisplayNameResolved = $AXI.DisplayNameRaw
+				if ($AXI.DisplayNameRaw -match '^ms-resource\:') {
+					Write-Verbose = 'For the want of an @, a kingdom is lost.'
+				}
+			}
+		} catch {
+			Write-Verbose "$($AXN): There are no display names associated with this package."
+		}
+	}
+
+	#Hand over the gather info to the array
+	$AppxIdentities[$i] = $AXI
+}
+
+Pop-Location
+
+$AppxIdentities
