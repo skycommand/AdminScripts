596ccc3b53156936aed16ee95bfef1918493d7b5
skycommand <jadangerbrooks@hotmail.com>
2020-09-08 01:59:36 +0430

    Added help

--- (Code snippets)/Administrative privileges.ps1
+++
@@ -1,5 +0,0 @@
-function AmIAdmin {
-  $MyId=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-  $WindowsPrincipal=New-Object System.Security.Principal.WindowsPrincipal( $MyId )
-  return $WindowsPrincipal.IsInRole( [System.Security.Principal.WindowsBuiltInRole]::Administrator )
-}
